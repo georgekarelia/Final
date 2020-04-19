f95d727c2addb35d7b0cea8806529d544418548e
George Paranichev <egordon1212@gmail.com>
2020-04-20 01:59:32 +0300

    dobavlen umn

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
-<<<<<<< HEAD
-#include "stdafx.h"\
 #include "plus.h"
-=======
 #include "stdafx.h"
 #include "min.h"
->>>>>>> f577630f91fb47952d34ae85ee6a518a8e4eec75
+#include "del.h"
 
 int main()
 {
     int a;
 
-<<<<<<< HEAD
 a = plus(3,2);
-=======
 a = min(3,2);
->>>>>>> f577630f91fb47952d34ae85ee6a518a8e4eec75
+a = umn(3,2);
 
 
 
---
+++ umn.h
@@ -0,0 +1,6 @@
+#ifndef _UMN_H_
+#define _UMN_H_
+
+int umn(int a, int b);
+
+#endif
