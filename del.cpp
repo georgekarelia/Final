9cb321f21a305126285c1f4038017c471f4bcfb7
george2 <egordon1212@live.ru>
2020-04-20 01:52:06 +0300

    dobavlen del

--- del.cpp
+++ del.cpp
@@ -3,5 +3,5 @@
 
 int main(int a, int b)
 {
-	return a + b;
+	return a / b;
 }
--- main.cpp
+++ main.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
-<<<<<<< HEAD
-#include "stdafx.h"\
-#include "plus.h"
-=======
 #include "stdafx.h"
+#include "plus.h"
 #include "min.h"
->>>>>>> f577630f91fb47952d34ae85ee6a518a8e4eec75
+#include "del.h"
 
 int main()
 {
     int a;
 
-<<<<<<< HEAD
 a = plus(3,2);
-=======
 a = min(3,2);
->>>>>>> f577630f91fb47952d34ae85ee6a518a8e4eec75
+a = del(3,2);
+
 
 
 
