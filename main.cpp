bb91bdba700edb5758216c19516a8f7d345b6403
george2 <egordon1212@live.ru>
2020-04-20 01:08:05 +0300

    plus

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include "stdafx.h"
+#include "stdafx.h"\
+#include "plus.h"
 
 int main()
 {
     int a;
 
-
+a = plus(3,2);
 
 
 
---
+++ plus.cpp
@@ -0,0 +1,7 @@
+
+#include "plus.h"
+
+int main(int a, int b)
+{
+	return a * b;
+}
